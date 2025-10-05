e794c4a5bf5a99db45ea7b895581042a8ef0bc6b
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-10-05 14:10:32 +0530

    Basic Auth Added(username,password)

--- src/main/java/com/ecommerce/controller/ManagementController.java
+++ src/main/java/com/ecommerce/controller/ManagementController.java
@@ -1,4 +1,24 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.service.ManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/management")
 public class ManagementController {
+
+    @Autowired
+    private ManagementService managementService;
+
+    @GetMapping("/something")
+    public ResponseEntity<String> getSomething(){
+        String msg = "In Management Controller";
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
 }
+
