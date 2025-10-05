5d60d405381a3811968fb7ba36205315a2ca4f06
Shailaja kshirsagar <shailajakshirsagarr@gmail.com>
2025-10-05 14:10:42 +0530

    Basic Auth Added(username,password) (#1)
    -Spring security dependencies added
    -username and password set in app.properties

--- src/main/java/com/ecommerce/controller/CustomerController.java
+++ src/main/java/com/ecommerce/controller/CustomerController.java
@@ -1,4 +1,24 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/something")
+    public ResponseEntity<String> getSomething(){
+        String msg = "In Customer Controller";
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/ecommerce/controller/SelletController.java
+++ src/main/java/com/ecommerce/controller/SelletController.java
@@ -1,4 +1,24 @@
 package com.ecommerce.controller;
 
+import com.ecommerce.service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/seller")
 public class SelletController {
+
+    @Autowired
+    private SellerService sellerService;
+
+    @GetMapping("/something")
+    public ResponseEntity<String> getSomething(){
+        String msg = "In Seller Controller";
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+
 }
