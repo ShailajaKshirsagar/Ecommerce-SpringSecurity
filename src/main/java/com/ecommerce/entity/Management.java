182e3d5c16e2e7c57111384a9ef6339a9dc3504a
Shailaja kshirsagar <shailajakshirsagarr@gmail.com>
2025-10-09 16:50:19 +0530

    Management api implemented (#3)

--- src/main/java/com/ecommerce/entity/Management.java
+++ src/main/java/com/ecommerce/entity/Management.java
@@ -1,5 +1,6 @@
 package com.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,7 @@ public class Management {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private int id;
 
     private String name;
--- src/main/java/com/ecommerce/serviceImpl/ManagementServiceImpl.java
+++ src/main/java/com/ecommerce/serviceImpl/ManagementServiceImpl.java
@@ -1,8 +1,50 @@
 package com.ecommerce.serviceImpl;
 
+import com.ecommerce.entity.Management;
+import com.ecommerce.repository.ManagementRepo;
 import com.ecommerce.service.ManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ManagementServiceImpl implements ManagementService {
+
+    @Autowired
+    private ManagementRepo managementRepo;
+
+    @Override
+    public String saveManager(Management management) {
+        managementRepo.save(management);
+        return "Manager Saved";
+    }
+
+    @Override
+    public Management getById(int id) {
+
+       Management management = managementRepo.findById(id).orElseThrow(()-> new RuntimeException("Id not found"));
+        return management;
+    }
+
+    @Override
+    public List<Management> getAllData() {
+        List<Management> managementList = managementRepo.findAll();
+        return managementList;
+    }
+
+    @Override
+    public String deleteById(int id) {
+        managementRepo.deleteById(id);
+        return "Manager deleted";
+    }
+
+    @Override
+    public Management updateManager(int id, Management management) {
+        Management manager1 = managementRepo.findById(id).orElseThrow(() ->  new RuntimeException("Customer not found"));
+        manager1.setCity(management.getCity());
+        manager1.setName(management.getName());
+        Management updatedManager = managementRepo.save(manager1);
+        return updatedManager;
+    }
 }
