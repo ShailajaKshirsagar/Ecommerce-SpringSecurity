a1a58380148f6285db27208c2db15344be66c48f
Shailaja kshirsagar <shailajakshirsagarr@gmail.com>
2025-10-15 19:18:48 +0530

    Flyway implemented (#8)

---
+++ src/main/resources/db/migration/V2__create_tables.sql
@@ -0,0 +1,25 @@
+
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE user_role (
+    user_id INT NOT NULL,
+    roles VARCHAR(50) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE seller (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE management (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL
+);
